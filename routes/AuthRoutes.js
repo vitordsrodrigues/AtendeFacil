7554b98dbeb0f7ecb4f3e0e614d7bbc0999d20c4
vitordsrodrigues <165427238+rodriguessilvavitor@users.noreply.github.com>
2025-09-14 16:11:24 -0300

    style(ui): estilização da página principal e dos formulários de login e registro

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const AuthController = require('../controllers/AuthController')
 
-router.post('/login', AuthController.login)
-router.post('/register', AuthController.register)
+router.get('/login', AuthController.login)
+router.get('/register', AuthController.register)
 
 
 module.exports = router
