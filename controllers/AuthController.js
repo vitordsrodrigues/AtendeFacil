3f91d3b4e30431c6de2b45d60f30337de6b8c6df
vitordsrodrigues <165427238+rodriguessilvavitor@users.noreply.github.com>
2025-09-20 13:51:50 -0300

    feat(auth): implementação do registro de usuários

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,11 +2,50 @@ const User = require('../models/User')
 
 module.exports = class AuthController {
 
+    static async login(req, res) {
+        res.render('auth/login')
+    }
+
     static register(req, res) {
         res.render('auth/register')
     }
-    static async login(req, res) {
-        res.render('auth/login')
+    
+    static async registerPost(req, res) {
+        const { name, email, password, confirmpassword } = req.body
+
+        //validações
+        if(password != confirmpassword) {
+            res.flash('message', 'As senhas não conferem, tente novamente.')
+            res.render('auth/register')
+            return
+        }
+
+        //verifica se o usuário já existe
+        const checkIfUserExists = await User.findOne({ where: { email: email } })
+        if(checkIfUserExists) {
+            res.flash('message', 'O e-mail já está em uso.')
+            res.render('auth/register')
+            return
+        }
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+        // Cria o usuário
+        const user = { name, email, password: hashedPassword }
+        try {
+            const createdUser = await User.create(user)
+
+            // Inicializa a sessão
+            req.session.userid = createdUser.id
+
+            res.flash('message', 'Cadastro realizado com sucesso!')
+            req.session.save(() => {
+                res.redirect('/')
+            })
+        } catch (error) {
+            console.error(error)
+            res.flash('message', 'Erro ao cadastrar usuário.')
+            res.render('auth/register')
+        }
     }
 
 }
